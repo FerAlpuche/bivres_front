5f85701779cd175ae842cbeba212fd3271793d2e
itsviejon <dcalflo8@gmail.com>
2021-06-21 23:39:56 -0500

    nuevo login

--- src/util/api.js
+++ src/util/api.js
@@ -1,4 +1,4 @@
-const SERVER_URL = "http://localhost:8090/";
+const SERVER_URL = "http://localhost:3000/";
 
 const instancia = axios.create({
   baseURL: SERVER_URL,
@@ -6,6 +6,20 @@ const instancia = axios.create({
 });
 
 export default {
+  async execute(method, resource, data, config) {
+    return instancia({
+      method: method,
+      url: resource,
+      data,
+      headers: {
+        Authorization:
+          localStorage.getItem("token") === null
+            ? ""
+            : "Bearer " + localStorage.getItem("token"),
+      },
+      config,
+    });
+  },
   doGet(endPoint) {
     return this.execute("GET", endPoint, null, {
       transformResponse: [
